1abc53724413c487b4e7c2e8f0accc451318579d
Munish <hmunish5@gmail.com>
2023-06-23 20:39:36 +0200

    Changed movie function to promise.all

--- promises/index.js
+++ promises/index.js
@@ -56,26 +56,19 @@ const getColdDrinks = new Promise((resolve, reject) => {
 })
 
 const movie = async function(){
-    try{let ticket = await person3Ticket;
+    try{
+    let [ticket, popcorn, butter, coldDrinks] = await Promise.all([person3Ticket, getPopcorn, addButter, getColdDrinks]);
 
   console.log(`got the ${ticket}`);
   console.log(`Husband:we should go in now`);
   console.log(`Wife: "i am hungry"`);
-  
-  let popcorn = await getPopcorn;
   console.log(`Husband: here is ${popcorn}`);
 	console.log(`Husband:we should go in now`);
   console.log(`Wife: "I dont like popcorn without butter!"`);
-  
-  let butter = await addButter;
   console.log(`added ${butter}`);
-   
   console.log(`Husband:Anything else darling`);
 	console.log(`Wife: lets go we are going to miss the preivew`);
   console.log(`Husband: thanks for the reminder *grin*`);
-
-  let coldDrinks = await getColdDrinks;
-
   console.log(`Husband: I got ${coldDrinks}`);
   
   return ticket;
